8eb4affe05ad5dbdcf5f2215eb58b8ca5e73189e
Fariviriski <mafu.is.here@gmail.com>
2024-01-08 15:04:49 -0400

    checkers for task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_Leaf - cheacks to see if a node is leaf
+ * @node: node to be checked
+ *
+ * Return: 1 if leaf, 0 if else
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return(0);
+	if(!(node->left) && !(node->right))
+		return(1);
+	return(0);
+}
