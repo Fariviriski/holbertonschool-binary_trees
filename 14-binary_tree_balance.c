4d3b81d093fb747a4f6fcd13c00835e97e2985a4
Fariviriski <mafu.is.here@gmail.com>
2024-01-10 09:31:02 -0400

    checks for 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_height_balance - measures balance factor of tree height
+ * @tree: pointer to root of tree
+ *
+ * Return: height of node NULL if void
+ */
+
+int binary_tree_height_balance(const binary_tree_t *tree)
+{
+	size_t h_left = 0, h_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		h_left = 1 + binary_tree_height_balance(tree->left);
+
+	if (tree->right)
+		h_right = 1 + binary_tree_height_balance(tree->right);
+
+	if (h_left > h_right)
+		return (h_left);
+
+	return (h_right);
+}
+
+
+/**
+ * binary_tree_balance - checks balance
+ * @tree: pointer to root node
+ *
+ * Return: balance of nodes
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t h_left = 0, h_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		h_left = 1 + binary_tree_height_balance(tree->left);
+
+	if (tree->right)
+		h_right = 1 + binary_tree_height_balance(tree->right);
+	return (h_left - h_right);
+}
